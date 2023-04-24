203912047dfd13d218c19f2c35b21985bd429702
Shiraz <Shiraz120010@gmail.com>
2023-04-24 12:30:05 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+
+
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
@@ -11,7 +14,7 @@ public class Main {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("yyxxy12233 = " + i);
+            System.out.println("yyxxy = " + i);
         }
     }
 }
