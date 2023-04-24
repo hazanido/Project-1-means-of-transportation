4046422012b4b4ddc45181a31082c6aaa2ffc3e8
ido Hazan <hazanido@gmail.com>
2023-04-24 12:29:07 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("yyxxy122323123 = " + i);
+            System.out.println("yyxxy12233 = " + i);
         }
     }
 }
