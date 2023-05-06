388f4dd206633ed617d96dd6dbc4e82d5f8c9701
ido Hazan <hazanido@gmail.com>
2023-05-06 20:49:02 +0300

    add

--- src/Graphic/dawn_rigoli.java
+++ src/Graphic/dawn_rigoli.java
@@ -1,4 +1,18 @@
 package Graphic;
 
-public class dawn_rigoli {
+import javax.swing.*;
+import java.awt.*;
+
+public class dawn_rigoli extends JFrame{
+
+    public dawn_rigoli (){
+        this.setVisible(true);
+        this.setSize(500, 100);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.getContentPane().setBackground(Color.gray);
+        this.setTitle("Type Vehicle");
+    }
 }
