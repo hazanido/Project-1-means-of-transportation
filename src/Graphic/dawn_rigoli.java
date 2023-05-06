cf60174bfa33ae79d0eb3b12b3537bd1b2743933
ido Hazan <hazanido@gmail.com>
2023-05-06 22:54:04 +0300

    add

--- src/Graphic/dawn_rigoli.java
+++ src/Graphic/dawn_rigoli.java
@@ -16,14 +16,30 @@ public class dawn_rigoli extends JFrame implements ActionListener {
         this.setLayout(null);
         this.getContentPane().setBackground(Color.gray);
         this.setTitle("Type Vehicle");
+        JLabel lebal = new JLabel("model:");
+        lebal.setBounds(70, 40, 150, 20);
+        this.add(lebal);
         model=new JTextField();
         model.setBounds(70,60,150,20);
+        model.setToolTipText("please enter model of car");
+        JLabel lebal1 = new JLabel("move:");
+        lebal1.setBounds(70, 90, 150, 20);
+        this.add(lebal1);
         move=new JTextField();
         move.setBounds(70,110,150,20);
+        JLabel lebal2 = new JLabel("distance:");
+        lebal2.setBounds(70, 140, 150, 20);
+        this.add(lebal2);
         distance=new JTextField();
         distance.setBounds(70,160,150,20);
+        JLabel lebal3 = new JLabel("passengers:");
+        lebal3.setBounds(70, 190, 150, 20);
+        this.add(lebal3);
         passengers=new JTextField();
         passengers.setBounds(70,210,150,20);
+        JLabel lebal4 = new JLabel("speed:");
+        lebal4.setBounds(70, 240, 150, 20);
+        this.add(lebal4);
         speed=new JTextField();
         speed.setBounds(70,260,150,20);
         this.add(model);
@@ -36,7 +52,7 @@ public class dawn_rigoli extends JFrame implements ActionListener {
 
 
     public void actionPerformed(ActionEvent e) {
-        return;
+
 
     }
 }
